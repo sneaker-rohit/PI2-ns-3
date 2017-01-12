da0b31da45478ec1b3f34869d993e229b9a807f8
rohittahiliani <rohit.tahil@gmail.com>
2017-01-13 01:07:48 +0530

    created a clone of PIE for the implementation of PI2

--- src/traffic-control/model/pi2-queue-disc.h
+++ src/traffic-control/model/pi2-queue-disc.h
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2016 NITK Surathkal
+ * Copyright (c) 2017 Trinity College Dublin
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -15,9 +15,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Authors: Shravya Ks <shravya.ks0@gmail.com>
- *          Smriti Murali <m.smriti.95@gmail.com>
- *          Mohit P. Tahiliani <tahiliani@nitk.edu.in>
+ * Authors: Rohit P. Tahiliani <rohit.tahil@gmail.com>
+ *          
+ *          
  */
 
 /*
